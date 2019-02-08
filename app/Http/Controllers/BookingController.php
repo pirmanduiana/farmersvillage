4e489950e4ec3bb03b2f524fdbb8f440f3f91aa5
pirmandwiana <pirmandwiana@gmail.com>
2019-02-08 17:08:22 +0800

    product order

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -12,7 +12,7 @@ class BookingController extends Controller
     //
     public function get_bestoffers()
     {
-        $products = Mstproduct::join('mst_currency', 'mst_product.currency_id', '=', 'mst_currency.id')->select('mst_product.*',DB::raw('mst_currency.code as currency_code'))->orderBy('mst_product.rating','desc')->get();
+        $products = Mstproduct::join('mst_currency', 'mst_product.currency_id', '=', 'mst_currency.id')->select('mst_product.*',DB::raw('mst_currency.code as currency_code'))->orderBy('mst_product.rating','desc')->orderBy('mst_product.price','desc')->limit(4)->get();
         $testimonies = Trntestimonial::all();
         
         return view('pages.booking', compact('products','testimonies'));
--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -11,7 +11,7 @@ class PackageController extends Controller
     //
     public function get_list()
     {
-        $products = Mstproduct::join('mst_currency', 'mst_product.currency_id', '=', 'mst_currency.id')->select('mst_product.*',DB::raw('mst_currency.code as currency_code'))->orderBy('mst_product.rating','desc')->get();
+        $products = Mstproduct::join('mst_currency', 'mst_product.currency_id', '=', 'mst_currency.id')->select('mst_product.*',DB::raw('mst_currency.code as currency_code'))->orderBy('mst_product.rating','desc')->orderBy('mst_product.price','desc')->get();
         
         return view('pages.packages', compact('products'));
     }
